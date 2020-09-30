5cc50b09f230261a14d9eb4bb6a70a600aa79103
TJ <tjspann@users.noreply.github.com>
2020-09-29 20:02:08 -0500

    Create en.lua

---
+++ esx_shops/locales/en.lua
@@ -0,0 +1,16 @@
+Locales['en'] = {
+  ['shop_robbery'] = 'Store robbery',
+  ['press_to_rob'] = 'Press ~INPUT_CONTEXT~ ~o~to rob~s~ ~b~%s~s~',
+  ['robbery_timer'] = 'Store robbery: he stays ~r~%s~s~ seconds',
+  ['recently_robbed'] = 'This store has already been robbed. Wait ~r~%s~b~ seconds.',
+  ['rob_in_prog'] = '~r~Robbery in progress at: ~b~%s~s~',
+  ['started_to_rob'] = 'You started to rob ~b~%s~s~',
+  ['alarm_triggered'] = 'The alarm has been triggered',
+  ['robbery_complete'] = '~r~Robbery Complete~s~. You have ~o~stolen~s~ ~g~$%s~s~',
+  ['robbery_complete_at'] = '~r~Robbery ended at: ~b~%s~s~',
+  ['robbery_cancelled'] = 'The robbery will be canceled, you will not gain anything!',
+  ['robbery_cancelled_at'] = '~r~Robbery canceled at: ~b~%s~s~',
+  ['min_police'] = 'It takes minimum ~b~%s Police~s~ in town to rob.',
+  ['robbery_already'] = '~r~A robbery is already in progress.',
+  ['no_threat'] = 'You do not pose a threat to the store keeper'
+}
